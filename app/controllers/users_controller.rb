64d26cd5b0edaea3690247ee8a174d33863bd004
Riley Siebel <riley.siebel@c3energy.com>
2014-11-16 19:50:18 -0800

    user registration complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      render :text => "User Created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:screen_name, :password, :email)
   end
 end
